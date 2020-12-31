95f3917ef7f6c2e5690b63a753d92288f3f4fbda
Ahmed <Ahmed@DESKTOP-CR8UIMM>
2020-12-31 12:40:36 +0200

    finish_project

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,83 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Operation.Firts_num = Convert.ToInt32(textBox1.Text);
+            Operation.Oper = '+';
+            textBox1.Text = "";
+           
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Operation.Second_num1= Convert.ToInt32(textBox1.Text);
+            switch (Operation.Oper)
+            {
+                case '+':textBox1.Text=Convert.ToString ( Operation.sum(Operation.Firts_num, Operation.Second_num1));
+                    break;
+                case '-':
+                    Operation.sub(Operation.Firts_num, Operation.Second_num1);
+                    break;
+            }
+
+        }
     }
 }
--- Calculator/Operation.cs
+++ Calculator/Operation.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Operation
+     static class Operation
     {
+      static  int firts_num;
+      static   int Second_num;
+        static char oper;
+
+        public static int Firts_num { get => firts_num; set => firts_num = value; }
+        public static int Second_num1 { get => Second_num; set => Second_num = value; }
+        public static char Oper { get => oper; set => oper = value; }
+
+        public static int sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static int sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int mul(int x, int y)
+        {
+            return x * y;
+        }
+        public static int div(int x, int y)
+        {
+            return x / y;
+        }
+
     }
 }
